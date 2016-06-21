d8730c26cf21d6dff5ac60cded42bb526f55384a
Thiago Soares <godaum19@gmail.com>
2016-06-21 16:10:14 -0300

    created classifier and tokenizer

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log("Hello world!")
+var natural = require ('natural');
+
+var trainGroup = [
+	{
+		sentence: "i didn't like it",
+		classification: "negative"
+	},
+	{
+		sentence: "i expected more",
+		classification: "negative"
+	},
+	{
+		sentence: "i liked it",
+		classification: "positive"
+	},
+	{
+		sentence: "i liked it",
+		classification: "positive"
+	},
+	{
+		sentence: "i enjoyed it",
+		classification: "positive"
+	}
+]
+
+var testGroup =[
+	{
+		sentence: "didn't like it",
+		classification: "negative"
+	},
+	{
+		sentence: "it was boring",
+		classification: "negative"
+	},
+	{
+		sentence: "liked it",
+		classification: "positive"
+	},
+	{
+		sentence: "awesome!",
+		classification: "positive"
+	}
+]
+
+
+tokenizer = new natural.WordTokenizer();
+console.log(tokenizer.tokenize(""));
+
+classifier = new natural.BayesClassifier();
+for(var i=0; i<trainGroup.length; i++){
+	classifier.addDocument(trainGroup[i].sentence,trainGroup[i].classification);
+}
+
+classifier.train();
+
