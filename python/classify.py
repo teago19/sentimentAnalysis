114b4ae5e795556ad112d7545958f9626207d394
Pedro Dias <phds@cin.ufpe.br>
2016-06-28 23:33:17 -0300

    fixed entry data to be used with the classifier

--- python/classify.py
+++ python/classify.py
@@ -1,8 +1,15 @@
+import json
+import math
+
+import nltk
 from nltk.classify import NaiveBayesClassifier
-from nltk.stem import PorterStemmer
-import json, re, math
-import random
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+
+def word_feats(words):
+    return dict([(word, True) for word in words])
+
 
 def clean_text(item):
     patterns = [
@@ -16,63 +23,50 @@ def clean_text(item):
 
     return item
 
+
 stop_words = stopwords.words('english')
+
+
 def remove_stop_words(item):
     item['text'] = " ".join(
-            list(
-                set(item['text'].split(' ')) - set(stop_words)
-                )
-            )
+        list(
+            set(item['text'].split(' ')) - set(stop_words)
+        )
+    )
     return item
 
 
 f = open('../reviews.json', 'r')
 reviews = json.loads(f.read())
 
-
 # run porter stemmer through all texts
 stemmer = PorterStemmer()
-stem_text = lambda x: { 'class': x['class'], 
-                        'text': stemmer.stem(x['text']) }
+stem_text = lambda x: {'class': x['class'],
+                       'text': stemmer.stem(x['text'])}
 
 # clean text and remove empty items
 reviews = filter(lambda x: x != {}, reviews)
 reviews = map(stem_text, reviews)
 
-#remove stopwords
+# remove stopwords
 reviews = map(remove_stop_words, reviews)
 
 reviews = map(clean_text, reviews)
-#reviews = map(remove_stop_words, reviews)
+# reviews = map(remove_stop_words, reviews)
 
 pos_reviews = filter(lambda x: x['class'] == 'POSITIVE', reviews)
 neg_reviews = filter(lambda x: x['class'] == 'NEGATIVE', reviews)
 
 # get unique features
-pos_feat_set = set()
-neg_feat_set = set()
-#pos_features = []
-#neg_features = []
-for review in pos_reviews:
-    #pos_features.append(({'text':review['text']}, 'pos'))
-    for feat in review['text'].split(' '):
-        pos_feat_set.add(feat)
-
-for review in neg_reviews:
-    #neg_features.append(({'text':review['text']}, 'neg'))
-    for feat in review['text'].split(' '):
-        neg_feat_set.add(feat)
-
-# label features for tests
 pos_features = []
 neg_features = []
 for review in pos_reviews:
-    for feat in pos_feat_set:
-        pos_features.append(({'text':feat},'pos'))
+    pos_features.append((word_feats(review['text'].split(' ')), 'pos'))
 
 for review in neg_reviews:
-    for feat in neg_feat_set:
-        neg_features.append(({'text':feat},'neg'))
+    neg_features.append((word_feats(review['text'].split(' ')), 'neg'))
+
+# label features for tests
 
 # divide groups
 #   training: 0~400
@@ -81,28 +75,15 @@ for review in neg_reviews:
 neg_features = list(neg_features)
 pos_features = list(pos_features)
 
-pos_offset = int(math.floor(len(pos_reviews)*3/4))
-neg_offset = int(math.floor(len(neg_reviews)*3/4))
+pos_offset = int(math.floor(len(pos_reviews) * 3 / 4))
+neg_offset = int(math.floor(len(neg_reviews) * 3 / 4))
 
 training = pos_features[:pos_offset] + neg_features[:neg_offset]
 testing = pos_features[pos_offset:] + neg_features[neg_offset:]
 
-random.shuffle(training)
-random.shuffle(testing)
-
 # train classifier
-training_words = []
-for item in training:
-    for word in item[0]['text'].split(' '):
-        training_words.append({'class':item[1], 'text':word})
 
 classifier = NaiveBayesClassifier.train(training)
 
-right, wrong = 0, 0
-for item in testing:
-    classification = classifier.classify(item[0])
-    print classification
-    if classification == item[1]:
-        right += 1
-    else:
-        wrong += 1
+print 'accuracy:', nltk.classify.util.accuracy(classifier, testing)
+classifier.show_most_informative_features()
